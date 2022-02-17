cef2b248e6f43e95ef586416be43bca424e3fbc7
lsh0902 <hehesi2007@naver.com>
2022-02-17 14:55:40 +0900

    initial

---
+++ app/admin/posts.rb
@@ -0,0 +1,5 @@
+ActiveAdmin.register Post do
+
+  # permit_params :user_id, :title, :content, :published_at
+
+end
---
+++ app/controllers/packs_controller.rb
@@ -0,0 +1,7 @@
+class PacksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
